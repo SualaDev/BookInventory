f341f4889a5bd9b6c3eff66e061a1145ed3ee851
SualaDev <81395134+SualaDev@users.noreply.github.com>
2023-03-20 14:59:09 +0100

    created the book section

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schemas/book.schema';
+import { CreateStudentDto } from './dto/create-student-dto';
+import { UpdateStudentDto } from './dto/update-student-dto';
+import { Query as ExpressQuery } from 'express-serve-static-core';
 
-@Controller('book')
-export class BookController {}
+@Controller('books')
+export class BookController {
+  constructor(private bookService: BookService) {}
+
+  @Get()
+  async getAllBooks(@Query() query: ExpressQuery): Promise<Book[]> {
+    return this.bookService.findAll(query);
+  }
+
+  @Post()
+  async createBook(
+    @Body()
+    book: CreateStudentDto,
+  ): Promise<Book> {
+    return this.bookService.create(book);
+  }
+
+  @Get(':id')
+  async getBook(
+    @Param('id')
+    id: string,
+  ): Promise<Book> {
+    return this.bookService.FindId(id);
+  }
+
+  @Put(':id')
+  async updateBook(
+    @Param('id')
+    id: string,
+    @Body()
+    book: UpdateStudentDto,
+  ): Promise<Book> {
+    return this.bookService.updateId(id, book);
+  }
+
+  @Delete(':id')
+  async deleteBook(
+    @Param('id')
+    id: string,
+  ): Promise<Book> {
+    return this.bookService.deleteId(id);
+  }
+}
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Book } from './schemas/book.schema';
+import mongoose, { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Query } from 'express-serve-static-core';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  constructor(
+    @InjectModel(Book.name)
+    private bookModel: Model<Book>,
+  ) {}
+
+  async findAll(query: Query): Promise<Book[]> {
+    const resPerPage = 2;
+    const currentPage = Number(query.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+    const keyword = query.keyword
+      ? {
+          title: {
+            $regex: query.keyword,
+            $options: 'i',
+          },
+        }
+      : {};
+
+    const books = await this.bookModel
+      .find({ ...keyword })
+      .limit(resPerPage)
+      .skip(skip);
+    return books;
+  }
+
+  async create(book: Book): Promise<Book> {
+    const res = await this.bookModel.create(book);
+    return res;
+  }
+
+  async FindId(id: string): Promise<Book> {
+    const isValidId = mongoose.isValidObjectId(id);
+    if (!isValidId) {
+      throw new BadRequestException('Please enter correct Id');
+    }
+    const book = await this.bookModel.findById(id);
+
+    if (!book) {
+      throw new NotFoundException('Book Not Found');
+    }
+    return book;
+  }
+
+  async updateId(id: string, book: Book): Promise<Book> {
+    return await this.bookModel.findByIdAndUpdate(id, book, {
+      new: true,
+      runValidators: true,
+    });
+  }
+
+  async deleteId(id: string): Promise<Book> {
+    return await this.bookModel.findByIdAndDelete(id);
+  }
+}
